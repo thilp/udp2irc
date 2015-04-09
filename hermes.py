f696260e98f8f96561bb5c99b3d5f2b483e1deed
thilp <thilp@thilp.net>
2015-04-09 18:25:36 +0200

    allow more than one UDP port (source) and chan (destination)

--- hermes.py
+++ hermes.py
@@ -1,24 +1,32 @@
 import argparse
 import sys
-import os, os.path
+import os
+import os.path
 
-from twisted.internet.protocol import DatagramProtocol, ClientFactory
+from twisted.internet.protocol import DatagramProtocol, ReconnectingClientFactory
 from twisted.words.protocols import irc
 from twisted.internet import reactor, ssl
 from twisted.python import log
 
 
 class Echo(DatagramProtocol):
-    def __init__(self, ircbot):
+    def __init__(self, ircbot, chans):
         self.irc = ircbot
+        self.chans = chans
 
     def datagramReceived(self, datagram, addr):
-        chans = self.irc.factory.channels
-        for chan in filter(lambda k: chans[k], chans):
-            self.irc.msg(chan, datagram)
+        for chan in self.chans:
+            if self.irc.joined[chan]:
+                self.irc.msg(chan, datagram)
+            else:
+                log.err('trying to write from %s on non-joined chan %s' % (addr, chan))
 
 
 class IrcBot(irc.IRCClient):
+    def __init__(self):
+        self.bridges = {}
+        self.joined = {}
+
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
         log.msg("connected")
@@ -39,11 +47,14 @@ class IrcBot(irc.IRCClient):
 
         reactor.callLater(5, IrcBot.identify, self)
 
-        for chan in self.factory.channels:
-            self.join(chan)
+        log.msg('establishing bridges')
+        chan_set = set()  # we want to join each chan only once
+        for port, chans in self.factory.ports2chans:
+            chan_set.add(*chans)
+            self.bridges[port] = reactor.listenUDP(port, Echo(self, chans))
 
-        log.msg('establishing bridge')
-        self.bridge = reactor.listenUDP(self.factory.udp_port, Echo(self))
+        for chan in chan_set:
+            self.join(chan)
 
     def irc_unknown(self, prefix, command, params):
         log.msg('from network: %s %s %s' % (prefix, command, params))
@@ -51,28 +62,28 @@ class IrcBot(irc.IRCClient):
     def joined(self, channel):
         irc.IRCClient.joined(self, channel)
         log.msg('joining %s' % channel)
-        self.factory.channels[channel] = True
+        self.joined[channel] = True
 
     def left(self, channel):
         irc.IRCClient.left(self, channel)
         log.msg('leaving %s' % channel)
-        self.factory.channels[channel] = False
+        self.joined[channel] = False
 
     def privmsg(self, user, channel, message):
         irc.IRCClient.privmsg(self, user, channel, message)
 
         user = user.split('!', 1)[0]
 
+        msg = "I'm just a robot! Please ask a Vikidia sysadmin if you have any question."
+
         # Check to see if they're sending me a private message
         if channel == self.nickname:
-            msg = "I'm just a robot! Please ask a Vikidia sysadmin if you have any question."
             self.msg(user, msg)
             return
 
         # Otherwise check to see if it is a message directed at me
         if message.startswith(self.nickname + ":"):
-            msg = "%s: I'm just a robot! Please ask a Vikidia sysadmin if you have any question." % user
-            self.msg(channel, msg)
+            self.msg(channel, "%s: %s" % (user, msg))
 
     def irc_ERR_NICKNAMEINUSE(self, prefix, params):
         wanted = self.factory.nickname
@@ -80,10 +91,9 @@ class IrcBot(irc.IRCClient):
         self.msg('NickServ', 'GHOST %s %s' % (wanted, self.password))
 
 
-class IrcBotFactory(ClientFactory):
-    def __init__(self, udp_port, channels, nickname, password=None):
-        self.udp_port = udp_port
-        self.channels = dict(zip(channels, map(lambda _: False, channels)))
+class IrcBotFactory(ReconnectingClientFactory):
+    def __init__(self, ports2chans, nickname, password=None):
+        self.ports2chans = port2chans
         self.nickname = nickname
         self.password = password
 
@@ -95,31 +105,48 @@ class IrcBotFactory(ClientFactory):
         return bot
 
     def clientConnectionLost(self, connector, reason):
-        """Automatic reconnection"""
+        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
         log.err('IRC connection lost: %s' % reason)
-        log.msg('reconnecting ...')
-        connector.connect()
+        self.retry(connector)
+
 
     def clientConnectionFailed(self, connector, reason):
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
         log.err('IRC connection failed: %s' % reason)
         log.msg('aborting')
         reactor.stop()
 
 
-if __name__ == '__main__':
+def none(*args):
+    return not any(args)
+
+
+def main():
     ap = argparse.ArgumentParser(description="Make a bridge between a UDP port and an IRC channel.")
-    ap.add_argument('--udp', type=int, required=True, help="port where to read UDP packets")
+    ap.add_argument('--human-udp', type=int, help="port where to read UDP packets for human-readable data")
+    ap.add_argument('--json-udp', type=int, help="port where to read UDP packets for JSON data")
     ap.add_argument('--server', required=True, help='IRC host, as "HOST:PORT"')
-    ap.add_argument('--chan', action='append', required=True, help="IRC channels to visit (usable multiple times)")
+    ap.add_argument('--human-chan', action='append', help="IRC channels for human-readable output (use multiple times)")
+    ap.add_argument('--json-chan', action='append', help="IRC channels for JSON output (use multiple times)")
     ap.add_argument('--nick', required=True, help="nickname used by the bot on IRC")
     ap.add_argument('--pwd', help="password associated to the nickname, if any")
-    ap.add_argument('--tls', action='store_true', help="use a secure connection")
+    ap.add_argument('--tls', action='store_true', help="use a secure IRC connection")
     args = ap.parse_args()
 
     irc_host, irc_port = args.server.split(':')
     irc_port = int(irc_port)
 
-    f = IrcBotFactory(args.udp, args.chan, args.nick, args.pwd)
+    if none(args.human_udp, args.json_udp):
+        raise RuntimeError('no UDP port provided')
+
+    port2chans = zip([args.human_udp, args.json_udp], [args.human_chan, args.json_chan])
+    for port, chans in port2chans:
+        if port is None and chans:
+            raise RuntimeError('no UDP port corresponding to IRC chans %s' % ', '.join(chans))
+        if port and not chans:
+            raise RuntimeError('no IRC chans corresponding to UDP port %s' % port)
+
+    f = IrcBotFactory(port2chans, args.nickname, args.pwd)
 
     if args.tls:
         cf = ssl.optionsForClientTLS(unicode(irc_host))
@@ -139,3 +166,7 @@ if __name__ == '__main__':
             reactor.run()
         finally:
             os.remove(lockfile)
+
+
+if __name__ == '__main__':
+    main()
